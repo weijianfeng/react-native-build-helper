6188a2122f38a6a26a2e4532a3d9f89ea6acdd12
Wei Jianfeng <676654309@qq.com>
2017-06-01 13:07:26 +0800

    commit copy-bundle command

--- bin/build-bundle.js
+++ bin/build-bundle.js
@@ -2,46 +2,35 @@
 
 var program = require('commander');
 var fs = require('fs');
-var path = require('path');
 var shell = require("shelljs");
-var exec = require('child_process').exec;
+var userdir  = require('os').homedir();
 
 program
-    .version('1.0.0')
+    .version('0.0.1')
     .option('-a, --android', 'build js-bundle for andrid')
     .option('-i, --ios', 'build js-bundle for ios')
     .option('-c, --clear', 'clear js-bundle')
     .parse(process.argv);
 
-// if (program.android) console.log('  - android');
 if (program.android) {
 
-  // if (! fs.existsSync('~/.rn')) {
-  //   fs.mkdirSync('~/.rn');
-  // }
-  //
-  // if (! fs.existsSync('~/.rn/assets')) {
-  //   fs.mkdirSync('~/.rn/assets');
-  // }
-  //
-  // if (! fs.existsSync('~/.rn/assets/android')) {
-  //   fs.mkdirSync('~/.rn/assets/android');
-  // }
-  //
-  // if (! fs.existsSync('~/.rn/res')) {
-  //   fs.mkdirSync('~/.rn/res');
-  // }
-  //
-  // if (! fs.existsSync('~/.rn/res/android')) {
-  //   fs.mkdirSync('~/.rn/res/android');
-  // }
+  if (! fs.existsSync(userdir + '/.rn')) {
+    fs.mkdirSync( userdir + '/.rn');
+  }
 
-  shell.exec('mkdir ~/.rn');
-  shell.exec('mkdir ~/.rn/assets/');
-  shell.exec('mkdir ~/.rn/assets/android/');
-  shell.exec('mkdir ~/.rn/res/');
-  shell.exec('mkdir ~/.rn/res/android/');
-  shell.exec('react-native bundle --platform android --dev false --entry-file index.android.js --bundle-output ~/.rn/assets/android/index.android.bundle --assets-dest ~/.rn/res/android/');
+  if (! fs.existsSync(userdir + '/.rn/android')) {
+    fs.mkdirSync(userdir + '/.rn/android');
+  }
+
+  if (! fs.existsSync(userdir + '/.rn/android/assets')) {
+    fs.mkdirSync(userdir + '/.rn/android/assets');
+  }
+
+  if (! fs.existsSync(userdir + '/.rn/android/res')) {
+    fs.mkdirSync(userdir + '/.rn/android/res');
+  }
+
+  shell.exec('react-native bundle --platform android --dev false --entry-file index.android.js --bundle-output ~/.rn/android/assets/index.android.bundle --assets-dest ~/.rn/android/res/');
 }
 if (program.ios) console.log('  - ios');
 if (program.clear) {
--- bin/copy-bundle.js
+++ bin/copy-bundle.js
@@ -1,4 +1,26 @@
 #! /usr/bin/env node
 
 var program = require('commander');
-var fs = require('fs');
+var fs = require('fs-extra');
+var userdir = require('os').homedir();
+
+program
+    .version('0.0.1')
+    .option('-a, --android', 'copy js-bundle to andrid project')
+    .option('-i, --ios', 'copy js-bundle to ios project')
+    .parse(process.argv);
+
+if (program.android) {
+    //  var userdir = process.env.HOME;
+    // console.log(userdir);
+    fs.copy(userdir + '/.rn/android/assets/index.android.bundle', 'src/main/assets/index.android.bundle', function(err) {
+    if (err) return console.error(err)
+    console.log("copy bundle success!")
+  });
+
+    fs.copy(userdir + '/.rn/android/res', 'src/main/res/', function(err) {
+    if (err) return console.error(err)
+    console.log("copy res success!")
+  });
+
+}
